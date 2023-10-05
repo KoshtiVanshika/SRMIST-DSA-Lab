b7c66c2f7b7eb25039355d87e773aab78e592802
Koshti Vanshika Shaileshbhai <140622612+KoshtiVanshika@users.noreply.github.com>
2023-10-05 16:29:12 +0530

    Add files via upload

---
+++ Infix expression to postfix expression/stack.h
@@ -0,0 +1,15 @@
+
+#ifndef STACK_H
+#define STACK_H
+
+struct Stack;
+
+struct Stack* createStack();
+void push(struct Stack* stack, char item);
+char pop(struct Stack* stack);
+char peek(struct Stack* stack);
+int isEmpty(struct Stack* stack);
+int isOperator(char c);
+int precedence(char op);
+
+#endif
